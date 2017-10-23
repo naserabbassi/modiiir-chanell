15de4fc4d46d104dcf0492b32b5e0631bbfa11c8
naserabbassi <32171222+naserabbassi@users.noreply.github.com>
2017-10-23 12:30:40 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,4 +1,4 @@
-bot_token = ""
+bot_token = "387199482:AAHXt4XKHXBc2pq5tPHjIfMPj3FflZRICnE"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "2.5"
-sudo_id = 212400472
+sudo_id = 129285906
